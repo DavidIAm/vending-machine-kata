b61e2dbab0795a265c5c4c1da5dbe0d44ad7f3fa
DAVIDIAM <davidihnen@gmail.com>
2016-01-20 06:20:59 +0000

    display now shows insert coin

--- lib/VMachine.pm
+++ lib/VMachine.pm
@@ -1,6 +1,7 @@
 package VMachine;
 
 use Moose;
+with 'VMachine::Display';
 
 our $VERSION='0.01';
 
---
+++ lib/VMachine/Display.pm
@@ -0,0 +1,22 @@
+package VMachine::Display;
+
+use Moose::Role;
+
+has display_text => (
+  is => 'rw',
+  isa => 'Str',
+  default => 'INSERT COIN',
+);
+
+sub set_display {
+  my ($self, $new) = @_;
+  $self->display_text(@_);
+  return $self->display_text();
+}
+
+sub read_display {
+  my ($self) = @_;
+  return $self->display_text();
+}
+
+1;
